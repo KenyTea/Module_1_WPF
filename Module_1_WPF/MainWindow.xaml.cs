b44d12246543f003f9eadc09c6f8da82d72980e6
Васильева <@223_16>
2018-06-05 21:51:45 +0600

    Add project files.

---
+++ Module_1_WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Module_1_WPF.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Module_1_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            Helper helper = new Helper();
+            string result = helper.SendMail(txbTo.Text, txbSubject.Text);
+            MessageBox.Show(result);
+        }
+    }
+}
---
+++ Module_1_WPF/Module/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace Module_1_WPF.Module
+{
+    public class Helper
+    {
+        public string SendMail(string to, string subject)
+        {
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("Vassilyevalexey@gmail.com", "Master Zen");
+
+            msg.To.Add(to);
+            msg.Subject = subject;
+            msg.Body = "Test Body";
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("Vassilyevalexey@gmail.com", "8888888"); //!!!!!
+
+            try
+            {
+                smtp.Send(msg);
+                return "Сообщение Отправлено";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        
+    }
+}
